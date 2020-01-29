7cc6d886eaba7be6d7be3bfb81d1cd6f21850947
sandeepsajan0 <sandeepsajan0@gmail.com>
2020-01-29 17:58:37 +0530

    add tests for all endpoint's method

--- tutorial_app/models.py
+++ tutorial_app/models.py
@@ -1,31 +1,42 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser,User
+from django.contrib.auth.models import AbstractUser, User
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import datetime
 
 # Create your models here.
 
+
 class User(AbstractUser):
     username = None
-    email = models.EmailField(_('email'), unique=True)
+    email = models.EmailField(_("email"), unique=True)
     name = models.CharField(max_length=250)
     avatar_url = models.URLField(blank=True, null=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
 
 class Idea(models.Model):
     """
     Ideas model
     """
+
     content = models.CharField(max_length=256, blank=False, null=False)
-    impact = models.IntegerField(validators=[MinValueValidator(1),
-                                             MaxValueValidator(10)],
-                                 blank=False, null=False)
-    ease = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],
-                               blank=False, null=False)
-    confidence = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)],
-                                     blank=False, null=False)
+    impact = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        blank=False,
+        null=False,
+    )
+    ease = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        blank=False,
+        null=False,
+    )
+    confidence = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        blank=False,
+        null=False,
+    )
     average_score = models.IntegerField(blank=True, null=False)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
