cf97c72952252279048d4c1e61c547b6b082949a
sandeepsajan0 <sandeepsajan0@gmail.com>
2020-02-05 15:53:35 +0530

    remove heroku settings

--- tutorial_1/settings.py
+++ tutorial_1/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from datetime import timedelta
-import django_heroku
 
-django_heroku.settings(locals())
+# import django_heroku
+#
+# django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
