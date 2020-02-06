6bf31604101546cd1e9be41467f2196af05c5bc7
sandeepsajan0 <sandeepsajan0@gmail.com>
2020-02-06 14:15:28 +0530

    add static dir to settings

--- tutorial_1/settings.py
+++ tutorial_1/settings.py
@@ -117,8 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # pagination
 REST_FRAMEWORK = {
